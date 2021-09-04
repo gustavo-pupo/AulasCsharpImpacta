46a523b6e1bd534be2d88ae25631e2f1c5e4f9e3
gustavo-pupo <gusfepupo@gmail.com>
2021-09-03 21:49:37 -0300

    commit 2 do dia

---
+++ CsharpCap1Aula2ValeTransporte/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CsharpCap1Aula2ValeTransporte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Inicio:
+
+            Console.WriteLine("Informe o nome do funcionário: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Agora informe o salário: ");
+            decimal salario = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Agora informe o valor gasto com transporte: ");
+            decimal gastoComTransporte = Convert.ToDecimal(Console.ReadLine());
+
+            decimal descontoMaximo = salario * 0.06m;
+            decimal descontoEfetivo = gastoComTransporte > descontoMaximo ? descontoMaximo : gastoComTransporte;
+
+            var resultado = $"\nFuncionário: {nome}" +
+                $"\n Salário: {salario:C}" +
+                $"\n Desconto Efetivo: {descontoEfetivo:C}";
+
+            Console.WriteLine(resultado);
+
+            Console.WriteLine("Pressone qualquer bosta para continuar ou Esc para sair");
+            /*ConsoleKeyInfo */var comando = Console.ReadKey();
+
+            if (comando.Key == ConsoleKey.Escape)
+            {
+                Environment.Exit(0);
+            }
+            Console.Clear();
+
+            //ToDo: Trocar o goto por uma estrutura de repetição
+            goto Inicio;
+
+
+        }
+    }
+}
