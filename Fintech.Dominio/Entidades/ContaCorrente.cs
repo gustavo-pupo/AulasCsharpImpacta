fa7f7baa694e927af0e42824404172897b77826a
gustavo-pupo <gusfepupo@gmail.com>
2021-09-09 21:23:54 -0300

    Cadastro de clientes

---
+++ Fintech.Dominio/Entidades/ContaCorrente.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fintech.Dominio.Entidades
+{
+    class ContaCorrente
+    {
+        public int Id { get; set; }
+        public Agencia Agencia { get; set; }
+        public int Numero { get; set; }
+        public string DigitoVerificador { get; set; }
+        public decimal Saldo { get; set; }
+        public Cliente Cliente { get; set; }
+
+        public void EfetuarOperacao(decimal valor, Operacao operacao)
+        {
+            switch (operacao)
+            {
+                case Operacao.Deposito:
+                    Saldo += valor;
+                    break;
+                case Operacao.Saque:
+                    Saldo -= valor;
+                    break;
+
+
+            }
+        }
+    }
+}
---
+++ FintechCorrentistaWpf/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using Fintech.Dominio;
+using Fintech.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace FintechCorrentistaWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Cliente> Clientes { get; set; } = new List<Cliente>();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            PopularControles();
+        }
+
+        private void PopularControles()
+        {
+            sexoComboBox.Items.Add(Sexo.Feminino);
+            sexoComboBox.Items.Add(Sexo.Masculino);
+            sexoComboBox.Items.Add(Sexo.Outro);
+
+            clienteDataGrid.ItemsSource = Clientes;
+
+        }
+
+        private void incluirClienteButton_Click(object sender, RoutedEventArgs e)
+        {
+            //int x = 10;//
+
+            Cliente cliente = new();
+            cliente.Cpf = cpfTextBox.Text;
+            cliente.DataNascimento = Convert.ToDateTime(dataNascimentoTextBox.Text);
+
+            var endereco = new Endereco();
+
+            endereco.Logradouro = logradouroTextBox.Text;
+            endereco.Cidade = cidadeTextBox.Text;
+            endereco.Cep = cepTextBox.Text;
+            endereco.Numero = numeroTextBox.Text;
+
+            cliente.Endereco = endereco;
+
+            cliente.Nome = nomeTextBox.Text;
+            cliente.Sexo = (Sexo)sexoComboBox.SelectedItem;
+
+            Clientes.Add(cliente);
+
+            MessageBox.Show("Cliente cadastrado com sucesso");
+
+            LimparControlesCliente();
+
+            clienteDataGrid.Items.Refresh();
+
+            pesquisaClienteTabItem.Focus();
+        }
+
+        private void LimparControlesCliente()
+        {
+            cpfTextBox.Clear();
+            nomeTextBox.Clear();
+            dataNascimentoTextBox.Clear();
+            logradouroTextBox.Clear();
+            cidadeTextBox.Clear();
+            cepTextBox.Clear();
+            numeroTextBox.Clear();
+            sexoComboBox.SelectedIndex = -1;
+            nomeTextBox.Clear();
+        }
+    }
+}
